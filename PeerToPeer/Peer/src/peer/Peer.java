f253a03c51094f5699c46e9cbcd26b405231c7ae
vladib13 <vladimirblanco13@gmail.com>
2017-12-28 15:48:48 -0300

    init

--- PeerToPeer/Peer/src/peer/Peer.java
+++ PeerToPeer/Peer/src/peer/Peer.java
@@ -4,6 +4,13 @@ import java.io.IOException;
 import java.net.ConnectException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  *
@@ -13,7 +20,9 @@ public class Peer {
 
    
     public static void main(String[] args) {
-        try{
+        RequestBody formBody = new FormBody.Builder().add("username","El administrador de redes de esos que llaman el maula, esos que les preguntas la configuracion basica de una red y te dicen CISCO").build();
+        Request r = new Request.Builder().url("http:/localhost:8888/viucab/Notificaciones/notificacionMail").post(formBody).build();
+        /*try{
         ServerSocket sc = new ServerSocket(5000); 
         int s = new String("192.168.1.100").hashCode();
         
@@ -25,6 +34,7 @@ public class Peer {
         }
         catch(IOException e){
             
-        }
+        }*/
+        
     }
 }
